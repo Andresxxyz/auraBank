2dfa12b619151aad7afef2c6541b0ca723fb5fb5
Andres <rafaelandres@gmail.com>
2025-10-15 15:01:40 -0300

    navbar

--- assets/php/conexao.php
+++ assets/php/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('DB_HOST') 
-$user = getenv('DB_USER') 
-$pass = getenv('DB_PASS') 
-$db   = getenv('DB_NAME')  
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME'); 
 $port = getenv('DB_PORT') ?: 3306;
 
 
